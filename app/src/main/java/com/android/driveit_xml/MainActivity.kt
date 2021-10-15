25c5c6605ec48b0ad8321819105ad9c4613b13cd
davidwekesar <davidwekesar@gmail.com>
2021-10-15 10:03:19 +0300

    first commit

---
+++ app/src/main/java/com/android/driveit_xml/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.android.driveit_xml
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
