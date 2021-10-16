cf441bc42e493c1eb8cc5402c6752efd6122cce4
davidwekesar <davidwekesar@gmail.com>
2021-10-16 12:36:42 +0300

    Add home fragment

--- app/src/main/java/com/android/driveit_xml/LoginFragment.kt
+++ app/src/main/java/com/android/driveit_xml/LoginFragment.kt
@@ -5,14 +5,23 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.google.android.material.button.MaterialButton
 
-class LoginFragment: Fragment() {
+class LoginFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        val view = inflater.inflate(R.layout.fragment_login, container, false)
+
+        val loginButton: MaterialButton = view.findViewById(R.id.loginButton)
+        loginButton.setOnClickListener {
+            findNavController().navigate(R.id.homeFragment)
+        }
+
+        return view
     }
 }
