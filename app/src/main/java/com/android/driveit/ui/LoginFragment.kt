414d3fa4d56608c88b257c4f971bb1b9bff20e2d
davidwekesar <davidwekesar@gmail.com>
2021-10-18 01:58:01 +0300

    Add firebase authentication to LoginFragment

--- app/src/main/java/com/android/driveit/ui/LoginFragment.kt
+++ app/src/main/java/com/android/driveit/ui/LoginFragment.kt
@@ -7,11 +7,15 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.NavOptions
 import androidx.navigation.fragment.findNavController
 import com.android.driveit.R
 import com.android.driveit.databinding.FragmentLoginBinding
+import com.android.driveit.viewmodels.FirebaseLoginStatus
 import com.android.driveit.viewmodels.LoginViewModel
 import com.google.android.material.button.MaterialButton
+
+data class UserInput(val email: String, val password: String)
 
 class LoginFragment : Fragment() {
 
@@ -26,14 +30,41 @@ class LoginFragment : Fragment() {
     ): View {
         _binding = FragmentLoginBinding.inflate(inflater, container, false)
 
+        initOnClickListeners()
+
+        initObservers()
+
+        return binding.root
+    }
+
+    private fun initOnClickListeners() {
         val loginButton: MaterialButton = binding.loginButton
         loginButton.setOnClickListener {
-            checkInputFields()
+            loginUser()
         }
 
+        val fireLoginButton: MaterialButton = binding.fireLoginButton
+        fireLoginButton.setOnClickListener {
+            loginFirebaseUser()
+        }
+    }
+
+    private fun initObservers() {
+        initLoginResponseObserver()
+
+        loginViewModel.firebaseLoginStatus.observe(viewLifecycleOwner, { firebaseLoginStatus ->
+            if (firebaseLoginStatus == FirebaseLoginStatus.SUCCESS) {
+                navigateToHomeFragment()
+            } else {
+                binding.editTextEmail.error = "Please enter the correct email or password."
+            }
+        })
+    }
+
+    private fun initLoginResponseObserver() {
         loginViewModel.loginResponse.observe(viewLifecycleOwner, {
             if (it != null) {
-                findNavController().navigate(R.id.homeFragment)
+                navigateToHomeFragment()
             } else {
                 Toast.makeText(
                     requireContext(),
@@ -42,17 +73,57 @@ class LoginFragment : Fragment() {
                 ).show()
             }
         })
-
-        return binding.root
     }
 
-    private fun checkInputFields() {
-        val email = binding.editTextEmail.text.toString().trim()
-        val password = binding.editTextPassword.text.toString().trim()
-
-        if (email.isNotBlank() && password.isNotBlank()) {
+    private fun loginUser() {
+        if (!isEmailAndPasswordEmpty()) {
+            val (email, password) = getUserEmailAndPassword()
             loginViewModel.loginUser(email, password)
         }
+    }
+
+    private fun loginFirebaseUser() {
+        if (!isEmailAndPasswordEmpty()) {
+            val (email, password) = getUserEmailAndPassword()
+            loginViewModel.fireLoginUser(email, password)
+        }
+    }
+
+    private fun navigateToHomeFragment() {
+        val navOptions = NavOptions.Builder().setPopUpTo(R.id.loginFragment, true).build()
+        findNavController().navigate(R.id.homeFragment, null, navOptions)
+    }
+
+    private fun isEmailAndPasswordEmpty(): Boolean {
+        val editTextEmail = binding.editTextEmail
+        val editTextPassword = binding.editTextPassword
+        val email = editTextEmail.text.toString().trim()
+        val password = editTextPassword.text.toString().trim()
+
+        return when {
+            email.isBlank() -> {
+                editTextEmail.error = "Email Required."
+                true
+            }
+            password.isBlank() -> {
+                editTextPassword.error = "Password Required."
+                true
+            }
+            email.isBlank() && password.isBlank() -> {
+                editTextEmail.error = "Email Required."
+                editTextPassword.error = "Password Required."
+                true
+            }
+            else -> {
+                false
+            }
+        }
+    }
+
+    private fun getUserEmailAndPassword(): UserInput {
+        val email = binding.editTextEmail.text.toString().trim()
+        val password = binding.editTextPassword.text.toString().trim()
+        return UserInput(email, password)
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/android/driveit/viewmodels/LoginViewModel.kt
+++ app/src/main/java/com/android/driveit/viewmodels/LoginViewModel.kt
@@ -6,15 +6,24 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.android.driveit.data.source.network.LoginResponse
 import com.android.driveit.repository.DriveItRepository
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import kotlinx.coroutines.launch
 import timber.log.Timber
+
+enum class FirebaseLoginStatus { SUCCESS, FAIL }
 
 class LoginViewModel : ViewModel() {
 
     private val repository = DriveItRepository()
+    private var auth: FirebaseAuth = Firebase.auth
 
     private val _loginResponse = MutableLiveData<LoginResponse>()
     val loginResponse: LiveData<LoginResponse> get() = _loginResponse
+
+    private val _firebaseLoginStatus = MutableLiveData<FirebaseLoginStatus>()
+    val firebaseLoginStatus: LiveData<FirebaseLoginStatus> get() = _firebaseLoginStatus
 
     fun loginUser(email: String, password: String) {
         viewModelScope.launch {
@@ -22,6 +31,16 @@ class LoginViewModel : ViewModel() {
                 _loginResponse.value = repository.loginUser(email, password)
             } catch (e: Exception) {
                 Timber.e("Error logging in user: ${e.message}")
+            }
+        }
+    }
+
+    fun fireLoginUser(email: String, password: String) {
+        auth.signInWithEmailAndPassword(email, password).addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                _firebaseLoginStatus.value = FirebaseLoginStatus.SUCCESS
+            } else {
+                _firebaseLoginStatus.value = FirebaseLoginStatus.FAIL
             }
         }
     }
