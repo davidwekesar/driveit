bb77db19a9794ab817e4d572d33df3ec32c4a85f
davidwekesar <davidwekesar@gmail.com>
2021-10-17 16:14:53 +0300

    Add bottom navigation view to MainActivity

--- app/src/main/java/com/android/driveit_xml/ui/MainActivity.kt
+++ app/src/main/java/com/android/driveit_xml/ui/MainActivity.kt
@@ -2,11 +2,72 @@ package com.android.driveit_xml.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import android.view.View
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.android.driveit_xml.R
+import com.android.driveit_xml.databinding.ActivityMainBinding
+import com.google.android.material.badge.BadgeDrawable
+import com.google.android.material.bottomappbar.BottomAppBar
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.shape.CornerFamily
+import com.google.android.material.shape.MaterialShapeDrawable
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var bottomNav: BottomNavigationView
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        bottomNav = binding.bottomNav
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+        navController = navHostFragment.navController
+
+        setupOnDestinationChangedListener()
+
+        addShapeToBottomNav()
+
+        addBadgeToBottomNav()
+    }
+
+    private fun addBadgeToBottomNav() {
+        val badge = bottomNav.getOrCreateBadge(R.id.account)
+        badge.isVisible = true
+        badge.number = 4
+        badge.badgeGravity = BadgeDrawable.BOTTOM_END
+    }
+
+    private fun addShapeToBottomNav() {
+        val radius = resources.getDimension(R.dimen.cornerSizeBottomNav)
+
+        val shapeDrawable: MaterialShapeDrawable = bottomNav.background as MaterialShapeDrawable
+        shapeDrawable.shapeAppearanceModel = shapeDrawable.shapeAppearanceModel.toBuilder()
+            .setTopLeftCorner(CornerFamily.ROUNDED, radius)
+            .setTopRightCorner(CornerFamily.ROUNDED, radius).build()
+    }
+
+    private fun setupOnDestinationChangedListener() {
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.loginFragment -> {
+                    bottomNav.visibility = View.GONE
+                }
+                R.id.carListFragment -> {
+                    bottomNav.visibility = View.GONE
+                }
+                else -> {
+                    bottomNav.visibility = View.VISIBLE
+                }
+            }
+        }
     }
 }
